d9e36cab92ff7c6d49c841e38fb1c7bf43d93dca
Alejandro-Val <134812072+Alejandro-Val@users.noreply.github.com>
2023-10-02 17:06:50 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default async function Home({ searchParams }: HomeProps) {
           <section>
             <div className='home__cars-wrapper'>
               {allCars?.map((car) => (
-                <CarCard car={car} />
+                <CarCard key={} car={car} />
               ))}
             </div>
 
